81dc96e202e9be95bd2e9b36c56328c1011284e6
KAD105 <kazem_abdi@yahoo.com>
2022-03-03 06:57:45 +0100

    helpful_scripts.py added

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,10 @@
+from operator import imod
+from charset_normalizer import from_fp
+from brownie import network, accounts, config
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
