78e35be879eca79b77b8061437bccdec68337487
KAD105 <kazem_abdi@yahoo.com>
2022-04-13 12:03:53 +0200

    _init_.py added

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,12 @@
-from brownie import FundMe, accounts
+from brownie import FundMe
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
+    fund_me = FundMe.deploy({"from": account})
+    print(f"Contract deployed to {fund_me.address}")
 
 
 def main():
-    deploy_fund_me
+    deploy_fund_me()
