7187e094d880e46c38ff05ca7818d46fe3c708c2
KAD105 <kazem_abdi@yahoo.com>
2022-04-15 09:59:33 +0200

    Project finished

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -6,10 +6,14 @@ from web3 import Web3
 DECIMALS = 8
 STARTING_PRICE = 200000000000
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "local-ganache"]
+FORKED_LOCAL_ENVIRONMENT = ["mainnet-fork", "mainnet-fork-dev"]
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENT
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
